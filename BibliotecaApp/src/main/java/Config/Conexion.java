7abd24dd56a2d97fb4eb5ec66129a6ad2a8f7164
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-06-16 12:01:31 -0500

    config conexion

---
+++ BibliotecaApp/src/main/java/Config/Conexion.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author kalic
+ */
+public class Conexion 
+{
+    
+    Connection connection = null;
+    String usuario = "sa";
+    String clave = "master123";
+    String bd = "Tienda";
+    String ip = "localhost";
+    String puerto = "1433";
+            
+    String url = "jdbc:sqlserver://"+ip+":"+puerto+"/"+bd;
+    
+
+    public Connection getConexion() {        
+        try {
+            String url = "jdbc:sqlserver://localhost:"+puerto+";"+"databaseName="+bd+";trustServerCertificate=true";
+            connection = DriverManager.getConnection(url, usuario, clave);
+            System.out.println("Conexión exitosa");
+        } catch (SQLException e) {
+            System.out.println("Error: Conexión fallida" + e.getMessage());
+        }
+
+        return connection;
+                
+    }
+
+    public void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println(" Cierre de conexión exitosa");
+            } catch (SQLException e) {
+                System.out.println("Error al cerrar la conexión" + e.getMessage());
+            }
+        }
+
+    }
+    
+}
