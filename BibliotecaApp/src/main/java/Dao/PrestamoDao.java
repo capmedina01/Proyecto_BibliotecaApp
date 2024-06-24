5330677d7a99a48b297b897af6412c602f4fbce8
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-06-24 11:19:23 -0500

    prestamo daO

--- BibliotecaApp/src/main/java/Dao/PrestamoDao.java
+++ BibliotecaApp/src/main/java/Dao/PrestamoDao.java
@@ -9,8 +9,12 @@ import Models.Prestamo;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -38,14 +42,63 @@ public class PrestamoDao {
             LocalDate fechaDevolucion = prestamo.getFechaDevolucion();
             Date sqlFechaDevolucion = Date.valueOf(fechaDevolucion);
             statement.setDate(4, sqlFechaDevolucion);
+            statement.executeUpdate();
        }
        catch(SQLException e){
-           System.out.println("error al crear el registro de prestamo" + e.getMessage());
-                    
-           
-       }
-        
+           System.out.println("error al crear el registro de prestamo" + e.getMessage());         
+       }        
     }
+    
+    public void actualizarPrestamo(Prestamo prestamo){
+        String sql ="UPDATE prestamo SET libroID=?, usuarioID=?, fechaPrestamo=?, fechaDevolucion=?, WHERE prestamoID=?";
+        try(PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setInt(1, prestamo.getLibroID());
+            statement.setInt(2, prestamo.getUsuarioID());
+            LocalDate fechaPrestamo = prestamo.getFechaPrestamo();
+            Date sqlFechaPrestamo = Date.valueOf(fechaPrestamo);
+            statement.setDate(3, sqlFechaPrestamo);
+            LocalDate fechaDevolucion = prestamo.getFechaDevolucion();
+            Date sqlFechaDevolucion = Date.valueOf(fechaDevolucion);
+            statement.setDate(4, sqlFechaDevolucion);
+            statement.executeUpdate();
+        }
+        catch(SQLException e){
+            System.out.println("Error al actualizar el registro" + e.getMessage());
+        }          
+    }
+    
+    public void eliminarPrestamo(int id){
+        String sql = "DELETE prestamo WHERE prestamoID =?";
+        try(PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setInt(0, id);
+            statement.executeUpdate();
+        }
+        catch(SQLException e){
+            System.out.println("Error al eliminar el registro" + e.getMessage());
+        }
+    }
+    
+    public List<Prestamo> mostrarPrestamo(){
+        List<Prestamo> listPrestamo = new ArrayList<>();
+        String sql ="SELECT * FROM prestamo";
+        try(Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql)){
+            while(resultSet.next()){
+                int id = resultSet.getInt("prestamoID");
+                int libroID = resultSet.getInt("libroID");
+                int usuarioID = resultSet.getInt("libroID");
+                
+                
+            }
+            
+        }
+        catch(SQLException e){
+            System.out.println("Error al mostrar los registros" + e.getMessage());
+        }
+        
+        return listPrestamo;
+    }
+    
     
     
 }
